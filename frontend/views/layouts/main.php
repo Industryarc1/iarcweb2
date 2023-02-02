98fccac66eafb44e69cc2c1032dc6f518150d3ba
RajeshIarc <117715644+RajeshIarc@users.noreply.github.com>
2023-02-02 15:43:23 +0530

    noindexing updated
    noindexing updated.

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -23,21 +23,15 @@ BaseAsset::register($this);
 <!--<html lang="<?= Yii::$app->language ?>">-->
 <html lang="en">
 <head>
+    <meta name="robots" content="noindex">
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="naver-site-verification" content="3e22cba18bf9a4dd210f94f71cabdc9a89191d69" />
 	<link rel="shortcut icon" href="<?= Yii::$app->request->baseUrl; ?>/favicon.ico" type="image/x-icon" />
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KNDMGMQ');</script>
-<!-- End Google Tag Manager -->
+
 
 <script> (function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('wVkO4XZb1wO8Z6Bj'); </script>
 
@@ -45,10 +39,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <body class="bg-white">
 <?php $this->beginBody() ?>
 
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KNDMGMQ"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
 
 <div class="wrap">
     <div class="container">
