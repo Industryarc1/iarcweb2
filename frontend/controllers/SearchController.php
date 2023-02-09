07aeea14ab7f00ce4184abaa86342f06ac0b129f
RajeshIarc <117715644+RajeshIarc@users.noreply.github.com>
2023-02-09 13:30:12 +0530

    qch1
    qch1

--- frontend/controllers/SearchController.php
+++ frontend/controllers/SearchController.php
@@ -55,13 +55,13 @@ class SearchController extends IarcfbaseController
 	public function actionSearchReport($reportTitle){
 		$keyTitle1= \Yii::$app->db->quoteValue($reportTitle.'%');
 		$keyTitle2= \Yii::$app->db->quoteValue($reportTitle);
-		
+		$newtitle = strtoupper(reportTitle);
 		/*$sql = "(SELECT `title`, `short_descr`, `curl`, `meta_title`, `dup_inc_id` FROM `zsp_posts` WHERE status=1 AND `title` LIKE ".$keyTitle1.") 
 				UNION 
 				(SELECT `title`, `short_descr`, `curl`, `meta_title`, `dup_inc_id` FROM `zsp_posts` WHERE status=1 AND MATCH(`title`) AGAINST (".$keyTitle2." IN NATURAL LANGUAGE MODE)) LIMIT 10";*/
 
 
-	$sql = "(SELECT `title`, `short_descr`, `description`, `curl`, `meta_title`, `dup_inc_id` FROM `zsp_posts` WHERE STATUS =1 AND (FROM_BASE64(`description`) LIKE '%$reportTitle%' OR title LIKE ".$keyTitle1.")) UNION (SELECT `title`, `short_descr`, `description`, `curl`, `meta_title`, `dup_inc_id` FROM `zsp_posts` WHERE STATUS=1 AND MATCH(`title`) AGAINST (".$keyTitle2." IN NATURAL LANGUAGE MODE)) LIMIT 50";	
+	$sql = "(SELECT `title`, `short_descr`, `description`, `curl`, `meta_title`, `dup_inc_id` FROM `zsp_posts` WHERE STATUS =1 AND (FROM_BASE64(`description`) LIKE '%$reportTitle%' OR FROM_BASE64(`description`) LIKE '%$newtitle%' OR title LIKE ".$keyTitle1.")) UNION (SELECT `title`, `short_descr`, `description`, `curl`, `meta_title`, `dup_inc_id` FROM `zsp_posts` WHERE STATUS=1 AND MATCH(`title`) AGAINST (".$keyTitle2." IN NATURAL LANGUAGE MODE)) LIMIT 50";	
 
 	//echo $sql;		
 
