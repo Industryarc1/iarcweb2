c8d95482566b6062305f27c2c2394f740c0e14c1
RajeshIarc <117715644+RajeshIarc@users.noreply.github.com>
2023-02-13 11:57:08 +0530

    popup back3
    popup back3

--- frontend/views/reports/report.php
+++ frontend/views/reports/report.php
@@ -686,8 +686,8 @@ foreach ($faqs as $key => $q) {
 </script>
 
 <div class="modal fade" id="notiModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document" style="background-color:blue">
-    <div class="modal-content">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content" style="background-color:blue">
       <div class="modal-header border-bottom-0">
         <a class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
