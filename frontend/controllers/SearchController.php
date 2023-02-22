99f1312f9291436652dc4d222a2481489c2d245d
RajeshIarc <117715644+RajeshIarc@users.noreply.github.com>
2023-02-22 17:11:38 +0530

    up ss q1
    up ss q1

--- frontend/controllers/SearchController.php
+++ frontend/controllers/SearchController.php
@@ -56,6 +56,7 @@ class SearchController extends IarcfbaseController
 		$keyTitle1= \Yii::$app->db->quoteValue($reportTitle.'%');
 		$keyTitle2= \Yii::$app->db->quoteValue($reportTitle);
 		$newtitle = strtoupper($reportTitle);
+		$newtitle2 = ucfirst($reportTitle);
 		/*$sql = "(SELECT `title`, `short_descr`, `curl`, `meta_title`, `dup_inc_id` FROM `zsp_posts` WHERE status=1 AND `title` LIKE ".$keyTitle1.") 
 				UNION 
 				(SELECT `title`, `short_descr`, `curl`, `meta_title`, `dup_inc_id` FROM `zsp_posts` WHERE status=1 AND MATCH(`title`) AGAINST (".$keyTitle2." IN NATURAL LANGUAGE MODE)) LIMIT 10";*/
@@ -65,7 +66,7 @@ class SearchController extends IarcfbaseController
 	
 	//main now  $sql = "(SELECT `title`, `short_descr`, `description`, `curl`, `meta_title`, `dup_inc_id` FROM `zsp_posts` WHERE STATUS =1 AND (FROM_BASE64(`description`) LIKE '%$reportTitle%' OR FROM_BASE64(`description`) LIKE '%$newtitle%' OR title LIKE ".$keyTitle1."))";
 
-	$sql = 	"(SELECT `title`, `short_descr`, `description`, `curl`, `meta_title`, `dup_inc_id` FROM `zsp_posts` WHERE STATUS =1 AND (REGEXP_REPLACE(FROM_BASE64(`description`), '<img[^>]*>', '') LIKE '%$reportTitle%' OR REGEXP_REPLACE(FROM_BASE64(`description`), '<img[^>]*>', '') LIKE '%$newtitle%' OR title LIKE ".$keyTitle1.") limit 50)";
+	$sql = 	"(SELECT `title`, `short_descr`, `description`, `curl`, `meta_title`, `dup_inc_id` FROM `zsp_posts` WHERE STATUS =1 AND (REGEXP_REPLACE(FROM_BASE64(`description`), '<img[^>]*>', '') LIKE '%$newtitle2%' OR REGEXP_REPLACE(FROM_BASE64(`description`), '<img[^>]*>', '') LIKE '%$reportTitle%' OR REGEXP_REPLACE(FROM_BASE64(`description`), '<img[^>]*>', '') LIKE '%$newtitle%' OR title LIKE ".$keyTitle1.") limit 50)";
 		
 	//echo $sql;		
 
