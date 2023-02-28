a5f52b08f8a3de6fbf37a9135a55ac0ea24a627a
RajeshIarc <117715644+RajeshIarc@users.noreply.github.com>
2023-02-28 21:32:49 +0530

    updated main-local
    updated main-local

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -7,6 +7,11 @@ return [
             'username' => 'iarcdbmain',
             'password' => 'vpfjeVCuRqm4#5c9AhPeDdG6mGWX!jY6',
             'charset' => 'utf8',
+            'enableSchemaCache' => true,
+            // Duration of schema cache.
+            'schemaCacheDuration' => 3600,
+            // Name of the cache component used to store schema information
+            'schemaCache' => 'cache',
         ],
 
         'mailer' => [
