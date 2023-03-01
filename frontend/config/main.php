00171cb812bcf012b46adf462247817def19edef
RajeshIarc <117715644+RajeshIarc@users.noreply.github.com>
2023-03-01 23:18:31 +0530

    main session up1
    main session up1

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -25,6 +25,7 @@ return [
         'session' => [
             // this is the name of the session cookie used for login on the frontend
             'name' => 'advanced-frontend',
+            'class' => 'yii\web\DbSession',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
