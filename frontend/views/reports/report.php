e457ca4a0f6a58871361ac01b8d64744f047acd5
RajeshIarc <117715644+RajeshIarc@users.noreply.github.com>
2023-03-02 17:34:43 +0530

    popup c2
    popup c2

--- frontend/views/reports/report.php
+++ frontend/views/reports/report.php
@@ -1,3 +1,5 @@
+<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.6.3/css/all.css'>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 <?php
 //use yii\helpers\Html;
 use frontend\helper\CommonHelper;
