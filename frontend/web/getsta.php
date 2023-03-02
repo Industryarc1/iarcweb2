7309bfc46c574dacb5da43c7c17d93378ca941c7
RajeshIarc <117715644+RajeshIarc@users.noreply.github.com>
2023-03-03 00:21:47 +0530

    lot7
    lot7

--- frontend/web/getsta.php
+++ frontend/web/getsta.php
@@ -10,14 +10,19 @@ $con=mysqli_connect("localhost","iarcdbmain","vpfjeVCuRqm4#5c9AhPeDdG6mGWX!jY6",
 
 		$query = "SELECT * FROM zsp_posts WHERE inc_id=2479";
 		$reportData = mysqli_query($con,$query);
-		$reportData = mysqli_fetch_assoc($reportData);
-		echo "<pre>";
-		print_r($reportData);
-		echo "</pre>";
+		//$reportData = mysqli_fetch_assoc($reportData);
 		
-		foreach ($reportData as $key => $rd) {
+		while($rows = mysqli_fetch_assoc($reportData)){
+			echo $rows["title"];			
+		}	
+
+		/*echo "<pre>";
+		print_r($reportData);
+		echo "</pre>";*/
+		
+		/*foreach ($reportData as $key => $rd) {
 			echo "<br>".$rd["title"];
-		}
+		}*/
 
 		
 		echo "connected!";
