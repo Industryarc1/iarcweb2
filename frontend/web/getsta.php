91c16db3a586d5cecc8aff5894f8cdd9b1126aa0
RajeshIarc <117715644+RajeshIarc@users.noreply.github.com>
2023-03-03 00:04:20 +0530

    lot up
    lot up

---
+++ frontend/web/getsta.php
@@ -0,0 +1,15 @@
+<?php
+//$conn = mysqli_connect("34.67.44.136","")
+
+$con=mysqli_connect("localhost","iarcdbmain","vpfjeVCuRqm4#5c9AhPeDdG6mGWX!jY6","iarcdb-live");
+	if (mysqli_connect_errno()){
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	else{
+
+		echo "connected!";
+	}
+
+
+
+?>
