85049afe8b38447ac19af51a85b69e2ae0ce5b37
RajeshIarc <117715644+RajeshIarc@users.noreply.github.com>
2023-03-06 21:36:34 +0530

    up pg3
    up pg3

--- frontend/views/purchase/viewLicenses.php
+++ frontend/views/purchase/viewLicenses.php
@@ -1,7 +1,311 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <?php
-
 use yii\helpers\Url;
-
-echo "hi...";
-
 ?>
+
+<!-- partial:index.partial.html -->
+<div class='container my-5'>
+  <div class='row pricing-intro'>
+    <div class='col-12 col-md-10 col-lg-8 mx-auto text-center mb-4'>
+      <h2>Select a license type that suits your business needs</h2>
+      <!-- <h3 class='sub-title'>Our service plans grow with your workforce! Compared to other MSPs, our solution offers more for less using a simple per-user monthly fee.</h3> -->
+    </div>
+  </div>
+  <div class='row d-block d-md-none pricing-plans d-flex justify-content-center mb-2'>
+    <div class='col-4 pricing-plan text-center' data-plan='basic-plan'>
+      <h4 class='py-3 mb-0'>Basic</h4>
+    </div>
+    <div class='col-4 pricing-plan text-center active-btn' data-plan='pro-plan'>
+      <h4 class='py-3 mb-0'>Advanced</h4>
+    </div>
+    <div class='col-4 pricing-plan text-center' data-plan='enterprise-plan'>
+      <h4 class='py-3 mb-0'>Expert</h4>
+    </div>
+  </div>
+  <div class='row pricing-table'>
+    <div class='col-6 col-md-3 px-0 col-compare'>
+      <div class='pricing-popular mb-2'></div>
+      <div class='pricing-single pricing-compare'>
+        <div class='title-row d-flex justify-content-start align-items-center px-3'>
+          <div class='title-wrap'>
+            <h2>Compare Plans</h2>
+            <!-- <h4 class='sub-title mb-0'>Additional fees may apply</h4> -->
+          </div>
+        </div>
+        <div class='cost-row d-flex justify-content-start align-items-center px-3'>
+          <div class='cost-wrap'>
+            <h3 class='cost mb-1'>Chapters Available</h3>
+          </div>
+        </div>
+<div class='compare-item-row d-flex justify-content-start align-items-center px-3'>
+          <span class='compare-title'>Overview</span></div>
+<div class='compare-item-row d-flex justify-content-start align-items-center px-3'>
+          <span class='compare-title'>Executive Summary</span></div>
+<div class='compare-item-row d-flex justify-content-start align-items-center px-3'>
+          <span class='compare-title'>Market Landscape</span></div>
+<div class='compare-item-row d-flex justify-content-start align-items-center px-3'>
+          <span class='compare-title'>Startup Company Scenario</span></div>
+<div class='compare-item-row d-flex justify-content-start align-items-center px-3'>
+          <span class='compare-title'>Market Entry Scenario</span></div>
+<div class='compare-item-row d-flex justify-content-start align-items-center px-3'>
+          <span class='compare-title'>Market Forces</span></div>
+<div class='compare-item-row d-flex justify-content-start align-items-center px-3'>
+          <span class='compare-title'>Strategic Analysis</span></div>
+<div class='compare-item-row d-flex justify-content-start align-items-center px-3'>
+          <span class='compare-title'>By Product & All Types listed</span></div>
+<div class='compare-item-row d-flex justify-content-start align-items-center px-3'>
+          <span class='compare-title'>By Geography</span></div>
+<div class='compare-item-row d-flex justify-content-start align-items-center px-3'>
+          <span class='compare-title'>By Application/End Use</span></div>
+<div class='compare-item-row d-flex justify-content-start align-items-center px-3'>
+          <span class='compare-title'>Market Entropy</span></div>
+<div class='compare-item-row d-flex justify-content-start align-items-center px-3'>
+          <span class='compare-title'>Market Share - Global Top 10</span></div>
+<div class='compare-item-row d-flex justify-content-start align-items-center px-3'>
+          <span class='compare-title'>Market Share by Country & Region</span></div>
+<div class='compare-item-row d-flex justify-content-start align-items-center px-3'>
+          <span class='compare-title'>Key Company List by Country</span></div>
+<div class='compare-item-row d-flex justify-content-start align-items-center px-3'>
+          <span class='compare-title'>Company Analysis</span></div>
+<div class='compare-item-row d-flex justify-content-start align-items-center px-3'>
+          <span class='compare-title'>Cross Segmentation</span></div>
+<div class='compare-item-row d-flex justify-content-start align-items-center px-3'>
+          <span class='compare-title'>By Geography x By Application</span></div>
+<div class='compare-item-row d-flex justify-content-start align-items-center px-3'>
+          <span class='compare-title'>By Geography x By Product</span></div>
+<div class='compare-item-row d-flex justify-content-start align-items-center px-3'>
+          <span class='compare-title'>By Geography x By Product x By Application </span></div>
+      </div>
+    </div>
+    <div class='d-none d-md-block col-6 col-md-3 px-0 col-pricing col-basic' id='basic-plan'>
+      <div class='pricing-popular mb-2'></div>
+      <div class='pricing-single pricing-basic'>
+        <div class='title-row d-flex justify-content-center align-items-center'>
+          <div class='title-wrap text-center'>
+            <h2>Basic</h2>
+            <!-- <h4 class='sub-title mb-0'>Essential services to resolve everyday tech headaches.</h4> -->
+          </div>
+        </div>
+        <div class='cost-row d-flex justify-content-center align-items-center'>
+          <div class='cost-wrap text-center'>
+            <h3 class='cost mb-1'>$4250</h3>
+            <span class='d-block sub-title fw-bold mb-3'>2 Days / Report Devlivery Timeline</span>
+            <a class='btn btn-teal'>Buy Now</a>
+          </div>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+        <i class="fa fa-check"></i>
+
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-times"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-times"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-times"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-times"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-times"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-times"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-times"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-times"></i>
+        </div>
+      </div>
+    </div>
+    <div class='d-none d-md-block col-6 col-md-3 px-0 col-pricing col-pro col-popular active-plan' id='pro-plan'>
+      <div class='pricing-popular active w-100 d-flex justify-content-center align-items-center mb-2'>
+        <h4 class='mb-0'>Most Popular</h4>
+      </div>
+      <div class='pricing-single pricing-pro'>
+        <div class='title-row d-flex justify-content-center align-items-center'>
+          <div class='title-wrap text-center'>
+            <h2>Advanced</h2>
+            <!-- <h4 class='sub-title mb-0'>Advanced services to meet the needs of small and mid-market businesses.</h4> -->
+          </div>
+        </div>
+        <div class='cost-row d-flex justify-content-center align-items-center'>
+          <div class='cost-wrap text-center'>
+            <h3 class='cost mb-1'>$5900</h3>
+            <span class='d-block sub-title fw-bold mb-3'>7 Days / Report Devlivery Timeline</span>
+            <a class='btn btn-teal'>Buy Now</a>
+          </div>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-times"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-times"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-times"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-times"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-times"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-times"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-times"></i>
+        </div>
+      </div>
+    </div>
+    <div class='d-none d-md-block col-6 col-md-3 px-0 col-pricing col-enterprise' id='enterprise-plan'>
+      <div class='pricing-popular mb-2'></div>
+      <div class='pricing-single pricing-enterprise'>
+        <div class='title-row d-flex justify-content-center align-items-center'>
+          <div class='title-wrap text-center'>
+            <h2>Expert</h2>
+            <!-- <h4 class='sub-title mb-0'>Enterprise services w/ reports, server backups, and advanced Microsoft 365 licensing.</h4> -->
+          </div>
+        </div>
+        <div class='cost-row d-flex justify-content-center align-items-center'>
+          <div class='cost-wrap text-center'>
+            <h3 class='cost mb-1'>$9500</h3>
+            <span class='d-block sub-title fw-bold mb-3'>12 - 15 Days / Report Devlivery Timeline</span>
+            <a class='btn btn-teal'>Buy Now</a>
+          </div>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+          <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+        <div class='compare-item-row d-flex justify-content-center align-items-center'>
+         <i class="fa fa-check"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- partial -->
